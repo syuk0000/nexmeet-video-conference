fad0c8869b9eac9efcf3c066960870dec85f3579
syuk0000 <0000.syuk@gmail.com>
2026-05-30 12:09:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const io = new Server(server, {
   maxHttpBufferSize: 5e6,
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const CAPTURES_DIR = path.join(__dirname, "captures");
 const ADMIN_PASSWORD = "lxvi2024"; // <-- APNA PASSWORD YAHAN BADLO
 
